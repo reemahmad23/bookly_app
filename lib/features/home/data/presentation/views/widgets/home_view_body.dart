0b9cecdf0142ce944bfa9ae1b4a37fb8d6384434
reemahmed <reemmahmed40@gmail.com>
2024-10-17 11:34:46 +0200

    create styles file and and title to home view

--- lib/features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@
 
 import 'package:bookly_app/features/home/data/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/features/home/data/presentation/views/widgets/featured_books_ist_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -13,28 +14,12 @@ class HomeViewBody extends StatelessWidget {
       children: [
         CustomAppBar(),
         FeaturedBooksListView(),
+        SizedBox(
+          height: 50,
+        ),
+        Text('Best Seller'),
+
       ],
     );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: CustomListViewItem(),
-          );
-        }
-      ),
-    );
-      
   }
 }
