9527ce2744b2d3b69f3a9356b5f0026356e8e31e
reemahmed <reemmahmed40@gmail.com>
2024-10-17 11:18:11 +0200

    create list view & add to home

--- lib/features/home/data/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/data/presentation/views/widgets/custom_list_view_item.dart
@@ -7,20 +7,17 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7/4 ,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: DecorationImage(
-              fit: BoxFit.fill ,
-              image: AssetImage(
-                AssetsDate.textImage
-                ),
-            ),
+    return AspectRatio(
+      aspectRatio: 2.7/4 ,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.red,
+          image: DecorationImage(
+            fit: BoxFit.fill ,
+            image: AssetImage(
+              AssetsDate.textImage
+              ),
           ),
         ),
       ),
