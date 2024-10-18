dc9e7420b3333f880b13ba465d574b3090338512
reemahmed <reemmahmed40@gmail.com>
2024-10-18 10:22:51 +0200

    add title and sub title to details

--- lib/features/home/data/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utlis/styles.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
@@ -12,10 +13,28 @@ class BookDetailsViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
       child: Column(
         children: [
-          CustomBookDetailsAppBar(),
+          const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .3),
-            child: CustomListViewItem(),
+            padding: EdgeInsets.symmetric(horizontal: width * .32),
+            child: const CustomListViewItem(),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text('Amarita & Zicola',
+          style: Styles.textStyle30,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text('Amr Abdelhamied',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+            ),
           ),
         ],
       ),
