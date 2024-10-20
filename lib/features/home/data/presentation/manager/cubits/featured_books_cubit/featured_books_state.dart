a0e66dcb68180994448ec0da3164dde7e035c35e
reemahmed <reemmahmed40@gmail.com>
2024-10-20 14:38:21 +0200

    create featured book state

---
+++ lib/features/home/data/presentation/manager/cubits/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,35 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+  
+    @override
+  // TODO: implement props
+  List<Object?> get props => [];
+
+
+}
+
+  class FeaturedBooksInitial extends FeaturedBooksState {
+
+  }
+  
+  class FeaturedBooksLoading extends FeaturedBooksState {}
+
+
+  class FeaturedBooksFailure extends FeaturedBooksState {
+    final String errMessage;
+
+  const FeaturedBooksFailure( this.errMessage);
+    
+  }
+
+  class FeaturedBooksSuccess extends FeaturedBooksState {
+
+    final List<BookModel> books;
+
+  const FeaturedBooksSuccess( this.books);
+
+    
+  }
